6b665015deb7ea5276260baddbd6ee61af7bc9ff
Virdin <73127621+VirdanTheBurden@users.noreply.github.com>
2021-01-20 16:47:24 -0500

    Update line_inverter.py

--- line_inverter.py
+++ line_inverter.py
@@ -22,6 +22,9 @@ def line_inverter(import_file_name, export_file_name, prefix_string):
     # creates a list with each line in a separate index
     line_list = f1.readlines()
     
+    # reverses line order
+    line_list.reverse()
+    
     # closes import file, since all data is stored in line_list
     f1.close()
     
@@ -32,27 +35,11 @@ def line_inverter(import_file_name, export_file_name, prefix_string):
         # strips the "\n" from the string
         line = i.strip("\n")
         
-        # inverts line string and overwrites to line var.
-        line = line[::-1]
-        
-        # splits line var. into individual characters and assigns to char_list
-        char_list = line.split()
-        
-        # loops through char_list
-        for i, char in enumerate(char_list):
-            
-            # concatenates prefix_string and a character from the list
-            x = prefix_string + char
-            
-            # assigns new variable (x) to the corresponding index the character
-            # came from
-            char_list[i] = x
-     
-        # joins char_list into a single string for export
-        line = (" ").join(char_list)
+        # concatenates prefix_string and a character from the line_list
+        x = prefix_string + line
         
         # appends the line to the new file
-        f2.write(line)
+        f2.write(x)
         
     # once the loop is finished, close the new file
     f2.close()
